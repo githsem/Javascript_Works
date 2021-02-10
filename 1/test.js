1e67dc5a088acd28d6fdc84c6393da95dbcdf901
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-02-10 12:13:42 +0100

    update

---
+++ 1/test.js
@@ -0,0 +1,11 @@
+console.log("Hosgeldin");
+var ad = "Hans";
+var list = ["A","B","C"];
+document.write("Sayfaya Hosgeldiniz : \n"+ad);
+document.write("\ndegisken : "+ typeof ad);   
+var s1 = 1;
+var s2 = 2;
+function topla(){
+    console.log(s1+s2);
+}
+topla();
